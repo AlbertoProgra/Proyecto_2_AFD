67ae43f81a8d395565e286bfa16fc302e5425944
oscarrrodriguez <oscar.rodriguez@galileo.edu>
2017-03-12 21:38:15 -0700

    Add files via upload

--- AfdMachine.java
+++ AfdMachine.java
@@ -1,58 +1,322 @@
+import java.io.File;
+import java.util.Scanner;
+import java.util.Vector;
+import java.util.HashMap;
 import java.io.*;
-import java.util.regex.Pattern;
-import java.util.regex.Matcher;
+
 
 public class AfdMachine{
+  int estado;
+  Vector<String> v;  
+  //String cadena;
+  String[] Alfabeto;
+  //String[] cadenatemp;
+  int cont;
+  int cont1;
+  int cont_estados;
+  int cont_estados2;
+  int cadenatam;
+  int es;
+
+//Constructor
+public AfdMachine(Vector<String> v, String[] Alfabeto){
+   estado = 1;
+   cont = 0;
+   cont_estados=0;
+   cont_estados2=0;
+   //cadenatemp = "";
+   this.v=v;
+   //this.cadena=cadena;
+   this.Alfabeto=Alfabeto;
+   new Vector<String>();
+} 
+
+//Metodo que devuelve el estado
+public int dameEstado(){
+   return estado;
+}
+
+//Metodo que devuelve el conteo de coincidencias
+public int dameConteo(){
+   return cont;
+}
+
+//Metodo que verifica las coincidencias de acuerdo al estado
+public void cambiaEstado(String cadena, int cadenatamanio){
+
+      cadenatam=cadenatamanio;
+		//System.out.println(cadena+"-recibe");
+
+		String[] AL = v.elementAt(1).split(",");
 	
-	static String name_afd;
-	static String op_1;
-	static String name_arch_cuerd;
+		for(int i=3; i<v.size();i++){
+            String[] temp = v.elementAt(i).split(",");
+           // System.out.println(v.elementAt(i));
+               for(int j=0; j<temp.length;j++){
+            // System.out.println(cadena+";"+AL[j]+":"+estado+":"+cont);	
+               // System.out.println(AL[j]);  
+             
+           if(cadena.equals(AL[j]) && temp[j].equals(String.valueOf(estado)))
+             {
+              // System.out.println(cadena+";"+AL[j]+":"+estado+":"+cont+"aqui");
+                              
+          estado=estado-1;
+           
+            //    System.out.println("Estado:"+estado);
 
-	public static String funcion_i(String afd, String cuerda){
-		String func1 =""; //Aca va la funcion cuando elegis -i (interactive)
-		return func1;
-	}
+              cont1++;
+             
+         //  System.out.println(cadena+";"+AL[j]+":"+estado+"----"+cont1);
+              
+              es = dameEstado()+1;
+             }
 
-	public static String funcion_b(String afd, String archivo){
-		String func2 ="";//Aca va la funcion cuando elegis -b (bach)
-		return func2;
-	}
 
-	//Clase principal
-	public static void main(String[] args) throws Exception {
-		int flag = 0;
-		String respuesta ="";
+         else if(cadena.equals(AL[j]) && temp[j].equals(String.valueOf(estado+1))){  
+                estado++;
+        //  System.out.println(cadena+";"+AL[j]+":"+estado);
+          	
+             	cont++;
+     // System.out.println(cadena+";"+AL[j]+":"+estado+"----"+cont);
+            // System.out.println("Conteo palabras:"+cont);
+             }
+  
+               	   
+               }//Fin de for de temp
 
-		//Manejo de errores
-		if ((args.length) != 0) {
-			if (args.length == 2 && args[1].equals("-i")) {
-				name_afd = args[0];
-				op_1 = args[1];
-				flag = 1;
-			} else if (args.length == 3 && args[1].equals("-b") && Pattern.matches(".+\\.txt",args[2])) {
-				name_afd = args[0];
-				op_1 = args[1];
-				name_arch_cuerd = args[2];
-				flag = 2;
-			} else {
-				System.out.println("[Parametros incorrectos] \n");
+     
+		} //Fin for de v.size()
+    
+
+	}  //Fin cambia estado
+//}//while
+
+
+//Metodo separa comas
+private static String[] separarcomas(String valor) {
+		return valor.split(",");
+}
+
+//Metodo que verifica si la cadena tiene los elementos del alfabeto
+public boolean Verificacadena(String cadena, String [] alfabeto) {
+   int cont = 0;
+      for(int x = 0 ; x < cadena.length(); x++){
+        String l = ""+cadena.charAt(x);
+           for(int y = 0 ; y < alfabeto.length ; y++){
+              if(l .equals(alfabeto[y])){
+                 cont++;
+        }
+      }
+    }
+    
+    if(cont == cadena.length()){
+      return true;
+    }else{
+      return false;
+    }
+    
+  }
+
+public String  resultado(){
+	String resul = "";
+	String[] EstadosFinales = v.elementAt(0).split(",");
+	for(int i=0; i<EstadosFinales.length;i++){
+	//	System.out.println("Estadosfinales "+EstadosFinales[i]);
+ //System.out.println("Estados "+dameEstado());
+ //System.out.println("conteo "+cont);
+ //  System.out.println("conteo2 "+cont1);
+  //  System.out.println("estadosestring "+es);
+
+         if((dameEstado())==Integer.parseInt(EstadosFinales[i])){
+             cont_estados++;
+         }else if (es==Integer.parseInt(EstadosFinales[i]))
+         {
+             
+             cont_estados2++;
+             cont_estados  = -1;
+
+        //     System.out.println("es"+es);
+         }
+
+	} //Fin de for
+
+
+
+	if(cont_estados>0 && cont==cadenatam){
+		resul = "ACEPTADA";
+    estado=1;
+    cont=0;
+    cont1=0;
+
+
+   //contestados=0;
+
+	}else if(es==4 && cont==cadenatam){
+		resul =  "ACEPTADA";
+    estado=1;
+    cont=0;
+    cont1=0;
+
+  // contestados=0;
+	}else {
+    resul =  "RECHAZADA";
+    estado=1;
+    cont=0;
+    cont1=0;
+
+  }
+
+
+
+   return resul;
+} //fin de Funcion
+
+
+
+//---------------------------------------------------------MAIN----------------------------------------------
+	public static void main(String[] args) {
+
+String  opcionb = "";
+String args2 = "";
+String args0 = "";
+String  opcioni = "";
+if(args.length==2 && args[1].equals("-i")){
+
+ args0 = args[0];
+ opcioni = args[1];
+
+}else if(args.length==3 && args[1].equals("-b")){
+ args0 = args[0];
+opcionb = args[1];
+args2 = args[2];
+}
+
+
+
+//-------------------------------------------------------MANEJO DE ARCHIVOS--------------------------------		
+
+// Fichero del que queremos leer
+File fichero = new File("afd/"+args[0]);//afd/hex.afd
+
+
+
+
+Scanner s = null;
+
+try {
+// Leemos el contenido del fichero
+
+s = new Scanner(fichero);
+
+
+// Leemos linea a linea el fichero
+String linea = null;
+int cont=0;
+
+//Se crea vector con tamaño x, para que almacene cada linea de la matriz ingresada
+Vector<String> v = new Vector<String>();
+Vector<String> vcadena = new Vector<String>();
+
+
+//Ciclo que recorre todas la lineas de la matriz y cada linea son guardas en vectores
+while (s.hasNextLine()) {
+	v.add (s.nextLine()); 	// Guardamos la linea en un vector
+	cont++; //Cuenta las lineas de la matriz
+}
+
+
+
+
+//-----------------------------------------------------------------------
+ //Estados que siempre van a tener el mismo indice
+ String[] EstadoFinal = separarcomas(v.elementAt(0));
+ String[] Alfabeto = separarcomas(v.elementAt(1));
+// String[] E0 = separarcomas(v.elementAt(2));
+//-----------------------------------------------------------------------
+String[] AL = v.elementAt(1).split(",");
+
+String Estados = new String(); 
+
+	AfdMachine ADF = new AfdMachine(v,  Alfabeto);
+
+
+//--------------------------------------------------LLAMA FUNCION QUE VERIFICA LA CADENA CON ALFABETO-----
+
+
+if(opcionb.equals("-b")){
+File ficherocadena = new File("afd/tests/"+args[2]);
+Scanner c = null;
+c = new Scanner(ficherocadena);
+
+
+
+while (c.hasNextLine()) {
+
+   String l = "";
+   String cadena =c.nextLine();   
+ // System.out.println(cadena);
+
+for(int i=0;i<cadena.length();i++){
+//System.out.println(cadena);
+  l = ""+cadena.charAt(i);
+  //System.out.println(l+"-envio");
+  
+ ADF.cambiaEstado(l, cadena.length());
+
+}
+System.out.println(ADF.resultado());
+
+}
+
+
+}else if(opcioni.equals("-i")){
+
+  while(true){
+
+Scanner entrada=new Scanner(System.in);
+
+String cadena = entrada.nextLine();
+
+if(cadena.equals("")){
+  break;
+}else{
+
+ String l = "";
+for(int i=0;i<cadena.length();i++){
+//System.out.println(cadena);
+  l = ""+cadena.charAt(i);
+  //System.out.println(l+"-envio");
+  
+ ADF.cambiaEstado(l, cadena.length());
+
+}
+System.out.println(ADF.resultado());
+}
+}
+
+} //fin else if
+
+
+
+
+
+//----------------------------------------------------------------------------------------------------------------------------
+		} catch (Exception ex) {
+			System.out.println("Mensaje: " + ex.getMessage());
+		} finally {
+			// Cerramos el fichero tanto si la lectura ha sido correcta o no
+			try {
+				if (s != null)
+					s.close();
+			} catch (Exception ex2) {
+				System.out.println("Mensaje 2: " + ex2.getMessage());
 			}
-		} else {
-			System.out.println("[No se ingreso ningun Parametro] \n");
 		}
 
-		//Llamada a funciones
-		if (flag != 0) {
-			BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-			String cuerda = bf.readLine();
-			if (flag == 1) {
-				if (cuerda.length() != 0) {
-					respuesta = funcion_i(args[0] , cuerda);
-				} //Se termina el programa debido a que cuerda == a 0
-			} else {
-				respuesta = funcion_b(args[0] , archivo);
-			}
-		}
-	System.out.println(name_afd + "\n" + op_1 + "\n"+ name_arch_cuerd);
+//XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+		
+
+
+
 	}
 }
